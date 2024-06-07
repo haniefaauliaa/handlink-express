82cd5a7b31b03537a578c00ce10213c4a72d586f
cello2599 <73047281+cello2599@users.noreply.github.com>
2024-06-07 17:07:02 +0700

    make css style upload and result page

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -8,47 +8,62 @@ const ViewImagePage = () => {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchImage = async () => {
-      try {
-        const res = await fetch('http://110.239.71.252:5003/get_gambar');
-        if (!res.ok) {
-          setImageUrl(null);
-          throw new Error('Failed to fetch image');
-        }
-        const buffer = await res.arrayBuffer();
-        const base64 = Buffer.from(buffer).toString('base64');
-        setImageUrl(`data:image/jpeg;base64,${base64}`);
-      } catch (error) {
-        setError(error instanceof Error ? error.message : 'Unknown error');
-      }
-    };
+      const fetchImage = async () => {
+          try {
+            const res = await fetch('http://127.0.0.1:5000/get_gambar');
+            if (!res.ok) {
+              setImageUrl(null);
+              throw new Error('Failed to fetch image');
+            }
+            const buffer = await res.arrayBuffer();
+            const base64 = Buffer.from(buffer).toString('base64');
+            setImageUrl(`data:image/jpeg;base64,${base64}`);
+          } catch (error) {
+            setError(error instanceof Error ? error.message : 'Unknown error');
+          }
+      };
 
     fetchImage();
   }, []);
 
   const handlePredict = async () => {
-    try {
-      const res = await fetch('http://110.239.71.252:5003/predict', { method: 'GET' });
-      if (!res.ok) {
-        throw new Error('Failed to predict');
+      try {
+        const res = await fetch('http://127.0.0.1:5000/predict', { method: 'GET' });
+        if (!res.ok) {
+          throw new Error('Failed to predict');
+        }
+        const data = await res.json();
+        setPrediction(data.predicted_text);
+      //   console.log(data.predicted_text);
+      } catch (error) {
+        setPrediction('An error occurred: ' + (error instanceof Error ? error.message : 'Unknown error'));
       }
-      const data = await res.json();
-      setPrediction(data.predicted_text);
-    //   console.log(data.predicted_text);
-    } catch (error) {
-      setPrediction('An error occurred: ' + (error instanceof Error ? error.message : 'Unknown error'));
-    }
-  };
+    };
 
-  return (
-    <div>
-      <h1>Uploaded Image</h1>
-      {error && <p>{error}</p>}
-      {imageUrl ? <img src={imageUrl} alt="Uploaded Image" /> : <p>No image available</p>}
-      <button onClick={handlePredict}>Predict</button>
-      {prediction && <p>{prediction}</p>}
-    </div>
-  );
+    return (
+      <main className="relative overflow-hidden bg-white">
+        <div className="container mx-auto flex min-h-[calc(100vh-92px)] flex-col gap-6 p-6 md:gap-24 md:py-24 lg:gap-24">
+            <div className="flex justify-center">
+                <div>
+                  <h1 className="rounded-2xl bg-blue-600 px-6 py-2 text-center text-xl font-bold text-white sm:text-2xl md:text-3xl">
+                    Uploaded Image
+                  </h1>
+                  {error && <p>{error}</p>}
+                  <div className="mt-4 flex text-sm leading-relaxed">
+                    {imageUrl ? <img src={imageUrl} alt="Uploaded Image" /> : <p>No image available</p>}
+                  </div>
+                  <div className="mt-4 flex justify-center">
+                    <button className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none 
+                    active:opacity-[0.85] active:shadow-none" onClick={handlePredict}>Predict</button>
+                  </div>
+                  <div className="mt-2">
+                    {prediction && <textarea className='border-0 shadow-sm ring-1'>{prediction}</textarea>}
+                  </div>
+                </div>
+              </div>
+            </div>
+      </main>
+    );
 };
 
 export default ViewImagePage;
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -12,7 +12,7 @@ const Page = () => {
     useEffect(() => {
         const prepareServer = async () => {
             try {
-              const response = await fetch('http://110.239.71.252:5003/prepare', { method: 'GET' });
+              const response = await fetch('http://127.0.0.1:5000/prepare', { method: 'GET' });
               if (!response.ok) {
                 throw new Error('Failed to prepare server');
               }
@@ -41,7 +41,7 @@ const Page = () => {
         formData.append('file', selectedFile);
 
         try {
-        const response = await fetch('http://110.239.71.252:5003/post_gambar', {
+        const response = await fetch('http://127.0.0.1:5000/post_gambar', {
                 method: 'POST',
                 body: formData,
             });
@@ -60,14 +60,34 @@ const Page = () => {
     };
 
   return (
-    <div>
-        <h1>Upload Image</h1>
-        <form onSubmit={handleSubmit}>
-            <input type="file" onChange={handleFileChange} />
-            <button type="submit">Upload</button>
-        </form>
-        {message && <p>{message}</p>}
-    </div>
+    <main className="relative overflow-hidden bg-white">
+        <div className="container mx-auto flex min-h-[calc(100vh-92px)] flex-col gap-6 p-6 md:gap-24 md:py-24 lg:gap-24">
+            <div className="flex justify-center">
+                <div className="flex max-w-screen-md flex-col items-center gap-6 bg-slate-600 rounded-md">
+                    <h1 className='text-white sm:text-2xl md:text-3xl'>Upload Image</h1>
+                    <div className="col-span-full">
+                            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                                <div className="text-center">
+                                    <svg className="mx-auto h-12 w-12 text-white" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
+                                    </svg>
+                                        <div className="mt-4 flex text-sm leading-6 text-white">
+                                            <form onSubmit={handleSubmit} className='text-white items-center'>
+                                                <input type="file" onChange={handleFileChange} />
+                                                    <button type="submit">
+                                                        <span className="pl-1">Upload a file</span>
+                                                    </button>   
+                                            </form>
+                                        </div>
+                                    <p className="text-xs leading-5 text-white">PNG, JPG, GIF up to 10MB</p>
+                                    {message && <p className='text-white pl-1'>{message}</p>}
+                                </div>
+                                </div>
+                            </div>
+                </div>
+            </div>
+        </div>
+    </main>
     );
 
 };
